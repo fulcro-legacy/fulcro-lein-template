12c708e00012642249d4b1e6aad530b066c46dac
Tony Kay <tony.kay@gmail.com>
2017-09-12 11:12:32 -0700

    updated version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject fulcro/lein-template "1.0.0"
+(defproject fulcro/lein-template "1.0.1"
   :description "A bare-bones template for new Fulcro full-stack projects."
   :url "https://fulcrologic.github.io/fulcro"
   :license {:name "MIT"
