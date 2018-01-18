4fbf3a107c9384621ba8b9a6db6312d409b1abc0
Tony Kay <tony.kay@gmail.com>
2018-01-18 15:27:29 -0800

    fixed template preload

--- resources/leiningen/new/fulcro/shadow-cljs/src/main/fulcro_template/development_preload.cljs
+++ resources/leiningen/new/fulcro/shadow-cljs/src/main/fulcro_template/development_preload.cljs
@@ -4,5 +4,5 @@
 ; Add code to this file that should run when the initial application is loaded in development mode.
 ; shadow-cljs already enables console print and plugs in devtools if they are on the classpath,
 
-(js/console.log "Turning logging to :all (in boo.development-preload)")
+(js/console.log "Turning logging to :all (in {{name}}.development-preload)")
 (log/set-level :all)
