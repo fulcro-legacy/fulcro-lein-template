727c20dbb960dec8edc1ac49f625f863228cb5e2
Lauritz Hilsøe <mail+gh@lauritz.me>
2019-03-30 12:16:47 +0100

    Update client template to use make-fulcro-client

--- resources/leiningen/new/fulcro/src/main/app/client.cljs
+++ resources/leiningen/new/fulcro/src/main/app/client.cljs
@@ -1,30 +1,27 @@
 (ns {{name}}.client
   (:require [fulcro.client :as fc]
             [{{name}}.ui.root :as root]
-            [fulcro.client.network :as net]
-            [fulcro.client.data-fetch :as df]))
+            [fulcro.client.network :as net]))
 
-(defonce SPA (atom nil))
+(defonce app (atom nil))
 
 (defn mount []
-  (reset! SPA (fc/mount @SPA root/Root "app")))
+  (reset! app (fc/mount @app root/Root "app")))
 
 (defn start []
   (mount))
 
 (def secured-request-middleware
-  ;; The CSRF token is embedded via server_components/html.clj
+  ;; The CSRF token is embedded in the server_components/html.clj
   (->
     (net/wrap-csrf-token (or js/fulcro_network_csrf_token "TOKEN-NOT-IN-HTML!"))
     (net/wrap-fulcro-request)))
 
 (defn ^:export init []
-  (reset! SPA (fc/new-fulcro-client
-                :started-callback (fn [{{name}}]
-                                    (df/load {{name}} :all-users root/User))
+  (reset! app (fc/make-fulcro-client
                 ;; This ensures your client can talk to a CSRF-protected server.
                 ;; See middleware.clj to see how the token is embedded into the HTML
-                :networking {:remote (net/fulcro-http-remote
-                                       {:url                "/api"
-                                        :request-middleware secured-request-middleware})}))
+                {:networking {:remote (net/fulcro-http-remote
+                                        {:url                "/api"
+                                         :request-middleware secured-request-middleware})}}))
   (start))
